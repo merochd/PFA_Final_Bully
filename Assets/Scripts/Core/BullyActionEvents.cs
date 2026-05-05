5e458e93d95c0320ce5d3384504cc064662f9991
Xileane <merochd@gmail.com>
2026-05-05 21:14:56 +0200

    21:14

--- Assets/Scripts/Core/BullyActionEvents.cs
+++ Assets/Scripts/Core/BullyActionEvents.cs
@@ -4,7 +4,6 @@ namespace Bully.Core
 {
     public static class BullyEvents
     {
-        // Exemple : Signal quand le joueur change de zone ou de santé
         public static Action<int> OnHealthChanged;
         public static void TriggerHealthChanged(int currentHealth) => OnHealthChanged?.Invoke(currentHealth);
     }
